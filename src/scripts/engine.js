193fe402430bbe102e2bed27ecefdb99ea570e23
Carolina Viana <carolina.sov@gmail.com>
2023-12-17 20:26:20 -0300

    estilos + início engine

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,13 @@
-alert("Olá mundo!")
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio("src/audios/a.wav")
+
+const playTune = (key) => {
+    audio.src = `src/audios/${key}.wav`
+    audio.play();
+};
+
+pianoKeys.forEach((key => {
+    console.log(key.dataset.key);
+    key.addEventListener("click", ()=>playTune(key.dataset.key))
+}));
